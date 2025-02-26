beaf07cf9a7249b718da0c4c5cc15bc31c1f6275
Irhas <irhasm@outlook.com>
2025-02-26 22:35:44 +0700

    chore: init SvelteKit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<div class="hero bg-base-200 min-h-screen">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">We're Building Something Exciting!</h1>
+      <p class="py-6">Beam is in development! Our team is hard at work creating a seamless ride-hailing experience. Stay tuned for updates and get ready to ride with us soon! 🚀</p>
+      <button class="btn btn-primary">Nothing here yet</button>
+    </div>
+  </div>
+</div>
