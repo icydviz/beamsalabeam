4d801750bc05c38c8ce99732fad2930eccdeb13f
Irhas <irhasm@outlook.com>
2025-09-25 23:34:20 +0700

    feat(AppBar): added header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,8 @@
   import '../app.css'
   import { onMount } from 'svelte'
   import { HomePageState } from './state.svelte'
+  import AppBar from '$components/AppBar.svelte'
+  import Footer from '$components/Footer.svelte'
 
   let { children } = $props(),
     pageState = new HomePageState()
@@ -16,4 +18,11 @@
   <link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<AppBar />
+
+<div id="smooth-wrapper">
+  <div id="smooth-content">
+    {@render children?.()}
+    <Footer />
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,26 +2,28 @@
   import { website } from '$lib'
   import { l } from '$lib/shared.svelte'
   import MetaHome from '$components/MetaHome.svelte'
-  import Footer from '$components/Footer.svelte'
   import WorkSteps from '$components/WorkSteps.svelte'
 </script>
 
 <MetaHome />
 
-<section class="hero h-screen">
-  <div class="hero-content">
+<section class="hero min-h-screen p-top-safe-area">
+  <div class="hero-content gradient-1">
     <div class="max-w-md text-center">
       <h1 class="text-title gentium-book-plus">
         {l('motto1Title.verbPhrase')}
         <span class="text-secondary">{l('motto1Title.adjective')}</span>
       </h1>
-      <p>{l('motto1Subtitle')}</p>
+      <p class="text-primary-gradient">{l('motto1Subtitle')}</p>
       <WorkSteps />
       <div class="w-full img-decor-1">
         <img class="rounded" src={`${website}/hero-1_3694×2078.webp`} alt="Battlecard bot" width="3694" height="2078" />
       </div>
+      <div class="flex-center p-1">
+        <button class="btn" type="button">
+          <span>Nothing here yet</span>
+        </button>
+      </div>
     </div>
   </div>
 </section>
-
-<Footer />
