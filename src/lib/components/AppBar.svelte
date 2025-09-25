f386e729fa2b3ef289cc87af8609a02e02378fa0
Irhas <irhasm@outlook.com>
2025-09-25 23:35:51 +0700

    feat(AppBar): added header (#9)

---
+++ src/lib/components/AppBar.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import IconBrand from '$components/IconBrand.svelte'
+</script>
+
+<header class="app-bar bg-frosted-glass">
+  <div class="app-bar-title">
+    <a href="/" class="btn btn-ghost rounded-full p-0">
+      <IconBrand />
+      <h1 class="brand-name text-base m-0">Beamsalabeam</h1>
+    </a>
+  </div>
+</header>
--- src/lib/components/WorkSteps.svelte
+++ src/lib/components/WorkSteps.svelte
@@ -2,14 +2,14 @@
   import { l } from '$lib/shared.svelte'
 </script>
 
-<div class="work-steps-container">
-  <div class="work-steps-bar">
+<div class="flex-center p-1">
+  <div class="work-steps-bar bg-frosted-glass">
     <ol class="list work-steps">
       {#each Array.from({ length: 3 }), i}
         {@const n = i + 1}
         <li>
           <button type="button" class="btn btn-ghost">
-            <span class="text-inverted"><small>{n}. {l(`step${n}`)}</small></span>
+            <span><small>{n}. {l(`step${n}`)}</small></span>
           </button>
         </li>
       {/each}
